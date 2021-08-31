ee7ae4f7ca0759d7045a39f81bf01e59e2d0a958
Burak Özkan <burak.ozkan@path.com.tr>
2021-08-31 15:23:34 +0300

    add token and midleware

--- server/index.ts
+++ server/index.ts
@@ -5,6 +5,7 @@ import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import morgan from 'morgan';
+import routes from './routes';
 
 // Middleware
 
@@ -17,9 +18,7 @@ app.use(morgan('dev'));
 app.use(cookieParser());
 
 //Routes
-app.get('/', (req, res)=>{
-  res.json({msg : 'Selam Tivilopır Burak'})
-})
+app.use('/api', routes.authRouter)
 
 // Database
 import './config/database'
--- server/routes/authRoute.ts
+++ server/routes/authRoute.ts
@@ -1,8 +1,9 @@
 import  express from 'express';
 import authCtrl from '../controllers/authConrollers';
+import { validRegister } from '../middleware/valid';
 
 const router = express.Router();
 
-router. post('/register', authCtrl.register);
+router. post('/register', validRegister, authCtrl.register);
 
 export default router;
